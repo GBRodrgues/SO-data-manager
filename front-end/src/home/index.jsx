cd0e793f1d89027490a139554cd8f80a46176569
GBRodrgues <gabrielcr18@outlook.com>
2025-02-03 19:56:42 -0300

    função ArrowUp e ArrowDown

--- front-end/src/home/index.jsx
+++ front-end/src/home/index.jsx
@@ -15,6 +15,8 @@ const Terminal = () => {
   const [output, setOutput] = useState('Bem-vindo SO-Corro Terminal!\n');
   const [command, setCommand] = useState('');
   const [path, setPath] = useState('root');
+  const [comand_history] = useState([]);
+  let [indice_comand_h, setIndice] = useState(1);
   
   const updatePath = async () => {
     try{
@@ -28,11 +30,13 @@ const Terminal = () => {
 
   const handleCommand = async (e) => {
   updatePath();
+  console.log(e.key)
   if (e.key === 'Enter') {
     const trimmedCommand = command.trim();
     if (trimmedCommand) {
       console.log('Comando digitado:', trimmedCommand);
-
+      comand_history.push(trimmedCommand)
+      console.log(comand_history)
       // Separando o comando e o argumento
       const [commandName, ...argsArray] = trimmedCommand.split(' ');
       const args = { name: argsArray.join(' ') };  // Garantindo que o nome seja passado
@@ -60,7 +64,16 @@ const Terminal = () => {
       }
       setCommand('');
       updatePath();
+      setIndice(1);
     }
+  }
+  if (e.key === 'ArrowUp' && indice_comand_h < comand_history.length) {
+    setIndice(indice_comand_h += 1);
+    setCommand(comand_history.at(comand_history.length -indice_comand_h));
+  }
+  if (e.key === 'ArrowDown'  && indice_comand_h > 0) {
+    setIndice(indice_comand_h -= 1);
+    setCommand(comand_history.at(comand_history.length -indice_comand_h));
   }
 };
 
