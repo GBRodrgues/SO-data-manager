04f48ac62099597a32231fd65a8fa7aa644c0a06
Caio Costa <caiocosta182@gmail.com>
2025-02-06 15:34:35 -0300

    Adição dos comandos: echo, echo>>,head, tail e wc

--- back-end/src/models/Arquivo.js
+++ back-end/src/models/Arquivo.js
@@ -1,36 +1,35 @@
 class Arquivo {
-    constructor(nome, conteudo = '') {
-        this.nome = nome;
-        this.conteudo = conteudo;
-        // this.usuariosPermitidos = []; 
-        // this.usuariosPermitidos.push(usuariosPermitidos);//lista de usuários permitidos
-        //Permissão podemos inserir como atributo dessa classe, criando uma nova classe 'usuario' e inserindo cada usuário nessa lista de usuários permitidos
-    }
+  constructor(nome, conteudo = "") {
+    this.nome = nome;
+    this.conteudo = conteudo;
+    // this.usuariosPermitidos = [];
+    // this.usuariosPermitidos.push(usuariosPermitidos);//lista de usuários permitidos
+    //Permissão podemos inserir como atributo dessa classe, criando uma nova classe 'usuario' e inserindo cada usuário nessa lista de usuários permitidos
+  }
 
-    write(conteudo) {
-        this.conteudo = conteudo;
-    }
+  write(conteudo) {
+    this.conteudo = conteudo;
+  }
 
-    append(conteudo) {
-        this.conteudo += `\n${conteudo}`;
-    }
+  append(conteudo) {
+    this.conteudo += `\n${conteudo}`;
+  }
 
-    read() {
-        return this.conteudo;
-    }
+  read() {
+    return this.conteudo;
+  }
 
-    updateNome(nome){
-        this.nome = nome;
-    }
+  updateNome(nome) {
+    this.nome = nome;
+  }
 
-
-    getStats() {
-        return {
-            nome: this.nome,
-            size: this.conteudo.length,
-            lines: this.conteudo.split('\n').length
-        };
-    }
+  getStats() {
+    return {
+      nome: this.nome,
+      size: this.conteudo.length,
+      lines: this.conteudo.split("\n").length,
+    };
+  }
 }
 
 export default Arquivo;
